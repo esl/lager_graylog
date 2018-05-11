fe9ad3ecd98417179abe187602412b7e70553e94
Arkadiusz Gil <arkadiusz@arkgil.net>
2018-05-11 15:36:01 +0200

    Define type specs for options accepted by backend and formatter

--- src/lager_graylog.erl
+++ src/lager_graylog.erl
@@ -1,6 +1,6 @@
 -module(lager_graylog).
 
--export_type([host/0, port_number/0, mask/0]).
+-export_type([host/0, port_number/0, mask/0, backend_option/0]).
 
 -type host() :: inet:hostname().
 -type port_number() :: inet:port_number().
@@ -8,3 +8,8 @@
 %% log level mask - definition taken from lager
 -type mask() :: {mask, integer()}.
 
+-type backend_option() :: {host, host()}
+                        | {port, port_number()}
+                        | {level, lager:log_level()}
+                        | {formatter, module()}
+                        | {formatter_config, term()}.
